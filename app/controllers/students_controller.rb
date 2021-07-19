4262c5d83c457338097fe4fec220c1599d659121
angeloxenakis <angeloxenakis@gmail.com>
2021-07-19 11:31:37 -0500

    commit tree 9ae6c1096da17fa2e9d3e42a09b1efa115448a7c

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,17 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+
+    if params[:name]
+      students = Student.all.find { |student| student.first_name == params[:name] || student.last_name == params[:name]}
+    end    
+
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
